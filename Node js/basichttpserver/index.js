a8f74b12cbb25b91c151a83ac975cbd64b41d639
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-07-27 00:13:34 +0530

    jj

--- Node js/basichttpserver/index.js
+++ Node js/basichttpserver/index.js
@@ -15,31 +15,49 @@ const port = 8000;
 const fs = require('fs');
 //why am i declaring a constant , to read this index.js file we need something that can read and write files 
 //there is a module built into nodejs which is called fs and is used for reading writing into files 
- 
 
-function requestHandler(req,res){
+
+function requestHandler(req, res) {
     console.log(req.url);
-    res.writeHead(200,{'content-type': 'text/html'});
+    res.writeHead(200, { 'content-type': 'text/html' });
 
-    fs.readFile('./index.html', function(err,data){
-        //fs is file system , above fs is module (line no 15) 
-        //readFile is an inbult synchronous function 
-        //err displayes error and data represents the file data 
+    // fs.readFile('./index.html', function (err, data) {
+    //     //fs is file system , above fs is module (line no 15) 
+    //     //readFile is an inbult synchronous function 
+    //     //err displayes error and data represents the file data 
+    //     if (err) {
+    //         console.log('error', err);
+    //         return res.end('error')
+    //     }
+    //     return res.end(data);
+    // });//the first arguement it takes is the path of this file we are going to declare 
+    // res.end('<h1>gotcha</h1>');
+
+    let filePath;
+    switch(req.url){
+        case '/':
+            filePath = './index.html'
+            break;
+        case 'profile':
+            filePath = './profile.html'
+            break;
+        default:
+            filePath = './404.html'
+    }
+    fs.readFile(filePath,function(err,data){
         if(err){
             console.log('error',err);
-            return res.end('error')
+            return res.end('<h1>Errorrr!!</h1>')
         }
-        return res.end(data);
-    });//the first arguement it takes is the path of this file we are going to declare 
-    // res.end('<h1>gotcha</h1>');
+    })
 }
 
 const server = http.createServer(requestHandler);
 
 
-server.listen(port,function(err){
+server.listen(port, function (err) {
     //when we call server.listen what happens is this this listen is called with port arguement passed through it which corresponds to 8000, if or if does not run this function is called by default 
-    if(err){
+    if (err) {
         console.log(err);
         return;
     }
