4d00eab429e06809f2ffc39cf032bc7f88c32903
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-08-10 00:47:40 +0530

    json property

---
+++ Spring Boot/json property.java
@@ -0,0 +1,8 @@
+// @JsonProperty("1. symbol")
+//   public String symbol;
+//   @JsonProperty("2. quantity")
+//   public int quantity;
+//   @JsonProperty("3. purchaseDate")
+//   public String purchaseDate;
+
+// this is to show how the data should look in the json file on the left side
